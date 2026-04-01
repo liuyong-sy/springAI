e061a6c163a71a6b1a82f7680a19094d8237dd70
mocker <2533416112@qq.com>
2026-04-01 22:55:52 +0800

    创建后端项目

---
+++ ai-robot-spring/src/main/java/com/ly/ai/robot/spring/AiRobotSpringApplication.java
@@ -0,0 +1,13 @@
+package com.ly.ai.robot.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRobotSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiRobotSpringApplication.class, args);
+    }
+
+}
---
+++ ai-robot-spring/src/test/java/com/ly/ai/robot/spring/AiRobotSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ly.ai.robot.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiRobotSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
