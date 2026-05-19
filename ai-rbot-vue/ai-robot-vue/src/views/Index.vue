ec114fb721f9ff9f715cd6b08508672ddb09f3cd
南橘丶 <2533416112@qq.com>
2026-05-19 17:40:44 +0800

    聊天页面基础布局

--- ai-rbot-vue/ai-robot-vue/src/views/Index.vue
+++ ai-rbot-vue/ai-robot-vue/src/views/Index.vue
@@ -1,15 +1,24 @@
 <template>
-    <div>
-      <h1 class="text-3xl font-bold bg-yellow-500">
-        小哈 AI 机器人 - 首页
-      </h1>
-      <a-button type="primary">发送</a-button>
+  <div class="h-screen max-w-3xl mx-auto relative">
+    <!-- 聊天记录区域 -->
+    <div></div>
+    <!-- 提问输入框 -->
+    <div class="absolute bottom-0 left-0 w-full mb-5">
+      <div class="bg-gray-100 rounded-3xl px-4 py-3 mx-4 border border-gray-200">
+        <textarea 
+          v-model="message" 
+          placeholder="给小哈 AI 机器人发送消息"
+          class="bg-transparent border-none outline-none w-full text-sm resize-none min-h-[24px]" 
+          rows="2">
+        </textarea>
+      </div>
     </div>
-  </template>
-  
-  <script setup>
-  </script>
-  
-  <style scoped>
-  </style>
-  
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+// 输入的消息
+const message = ref('')
+</script>
